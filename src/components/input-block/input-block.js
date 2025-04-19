8e345d83fd885d24c9e472f99ddc2583512de9f1
Evan Ayalin <siyeel04@gmail.com>
2025-04-19 18:13:08 +0800

    Added almost all functionalities of input-block component

--- src/components/input-block/input-block.js
+++ src/components/input-block/input-block.js
@@ -1,3 +1,5 @@
+import DateHandler from "../../scripts/date-handler.js";
+
 function InputBlock({
     inputType = 'one',
     title = 'Input title',
@@ -25,14 +27,10 @@ function InputBlock({
     const fragment = CreateFragment(template);
     const component = fragment.querySelector('article.input-block');
 
-    let optionalComponent;
+    const input_label = component.querySelector('.input-label');
+    const optionalComponent = CreateOptional();
 
-    if (isOptional) {
-        const input_label = component.querySelector('.input-label');
-
-        optionalComponent = CreateOptional();
-        input_label.appendChild(optionalComponent);
-    };
+    input_label.appendChild(optionalComponent);
 
     let objectComponents
 
@@ -41,7 +39,7 @@ function InputBlock({
     } else if (inputType.toLowerCase() === 'multi') {
         objectComponents = CreateMultiLiner(component, inputId);
     } else if (inputType.toLowerCase() === 'time') {
-
+        objectComponents = CreateTimeLiner(component, inputId);
     } else {
         console.log(`Type argument (${inputType}) is not a proper type.`);
 
@@ -157,6 +155,31 @@ function InputBlock({
         };
     };
 
+    /**
+     * Removes all clickables
+     */
+    const removeClickables = () => {
+        if (hint.hasHint && hint.clickable && hintComponent) {
+            const button_group = hintComponent.querySelector('.button-group');
+
+            button_group.innerHTML = '';
+        };
+    };
+
+    const requiredMessage = (message = 'required') => {
+        if (!isOptional) {
+            optionalComponent.textContent = `(${message})`;
+            optionalComponent.classList.add('required');
+        };
+    };
+    
+    const removeRequiredMessage = () => {
+        if (!isOptional) {
+            optionalComponent.textContent = `(optional)`;
+            optionalComponent.classList.add('required');
+        };
+    };
+
     return {
         component,
         render,
@@ -165,7 +188,10 @@ function InputBlock({
         disable,
         showHint,
         hideHint,
-        addClickables
+        addClickables,
+        removeClickables,
+        requiredMessage,
+        removeRequiredMessage
     };
 };
 
@@ -226,8 +252,127 @@ function CreateMultiLiner(component, inputId) {
     }
 };
 
-function CreateTimeLiner() {
+/**
+ * Creates a time input block component
+ * @param {HTMLElement} component - Component to handle 
+ * @param {string} inputId - Component's id
+ */
+function CreateTimeLiner(component, inputId) {
+    const timeNow = DateHandler.currentTime().toString();
+    const dateNow = DateHandler.currentDate().toString();
 
+    const template =
+        `
+        <div class="time-format" id="${inputId}">
+            <input type="number" name="" id="hour-ten" value="${timeNow.slice(0, 1)}"><input type="number" name="" id="hour-one" value="${timeNow.slice(1, 2)}">:<input type="number" name="" id="minute-ten" value="${timeNow.slice(3, 4)}"><input type="number" name="" id="minute-one" value="${timeNow.slice(4, 5)}"><input class="btn" type="button" id="meridian" value="${timeNow.slice(5, 7).toLowerCase()}"> | <input type="number" name="" id="month-ten" value="${dateNow.slice(0, 1)}"><input type="number" name="" id="month-one" value="${dateNow.slice(1, 2)}">/<input type="number" name="" id="day-ten" value="${dateNow.slice(3, 4)}"><input type="number" name="" id="day-one" value="${dateNow.slice(4, 5)}">/<input type="number" name="" id="year-mille" value="${dateNow.slice(6, 7)}"><input type="number" name="" id="year-cent" value="${dateNow.slice(7, 8)}"><input type="number" name="" id="year-ten" value="${dateNow.slice(8, 9)}"><input type="number" name="" id="year-one" value="${dateNow.slice(9, 10)}">
+        </div>
+    `;
+
+    const fragment = CreateFragment(template);
+    const input = fragment.querySelector('.time-format');
+
+    const cont_inputWrapper = component.querySelector('.input-wrapper');
+
+    cont_inputWrapper.appendChild(input);
+
+    const input_meridian = component.querySelector('#meridian');
+    const arrow = cont_inputWrapper.querySelector('.arrow');
+    const timeInputs = [];
+    const input_ht = input.querySelector('#hour-ten');
+    const input_ho = input.querySelector('#hour-one');
+    const input_mt = input.querySelector('#minute-ten');
+    const input_mo = input.querySelector('#minute-one');
+    const input_mot = input.querySelector('#month-ten');
+    const input_moo = input.querySelector('#month-one');
+    const input_dt = input.querySelector('#day-ten');
+    const input_do = input.querySelector('#day-one');
+    const input_ym = input.querySelector('#year-mille');
+    const input_yc = input.querySelector('#year-cent');
+    const input_yt = input.querySelector('#year-ten');
+    const input_yo = input.querySelector('#year-one');
+
+    timeInputs.push(input_ht);
+    timeInputs.push(input_ho);
+    timeInputs.push(input_mt);
+    timeInputs.push(input_mo);
+    timeInputs.push(input_meridian);
+    timeInputs.push(input_mot);
+    timeInputs.push(input_moo);
+    timeInputs.push(input_dt);
+    timeInputs.push(input_do);
+    timeInputs.push(input_ym);
+    timeInputs.push(input_yc);
+    timeInputs.push(input_yt);
+    timeInputs.push(input_yo);
+
+    timeInputs.forEach(timeInput => {
+        const originalValue = timeInput.value;
+
+        timeInput.addEventListener('keydown', (e) => {
+            if (e.key === 'ArrowLeft') {
+                const index = timeInputs.indexOf(timeInput);
+
+                if (index - 1 !== -1) {
+                    timeInputs[index - 1].focus();
+                };
+            };
+
+            if (e.key === 'ArrowRight') {
+                const index = timeInputs.indexOf(timeInput);
+
+                if (index + 1 !== timeInputs.length) {
+                    timeInputs[index + 1].focus();
+                };
+            };
+        });
+
+        if (timeInput.id !== 'meridian') {
+            timeInput.addEventListener('input', () => {
+                const nums = '0123456789';
+                const value = timeInput.value;
+
+                if (!nums.includes(value.slice(1, 2))) return;
+
+                if (value.length > 1) {
+                    timeInput.value = value.slice(1, 2);
+                };
+
+                if (value.length === 0) {
+                    timeInput.value = originalValue;
+                };
+            });
+
+            timeInput.addEventListener('keydown', (e) => {
+                const value = timeInput.value;
+
+                if (e.key === '.' || e.key === '-') {
+                    setTimeout(() => {
+                        timeInput.value = value.slice(0, 1);
+                    }, 0);
+
+                    return;
+                };
+            });
+        };
+    });
+
+    input_meridian.addEventListener('click', () => {
+        input_meridian.value = input_meridian.value === 'am' ? 'pm' : 'am';
+    });
+
+    input_meridian.addEventListener('mousedown', () => {
+        if (arrow.classList.contains('stagnant')) arrow.classList.remove('stagnant');
+
+        arrow.classList.add('typing');
+    });
+
+    input_meridian.addEventListener('mouseup', () => {
+        arrow.classList.remove('typing');
+    });
+
+    return {
+        input
+    };
 };
 
 /**
