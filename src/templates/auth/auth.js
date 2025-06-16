ea07b4559c5672523eaebabaf369d4ba3c78e34c
Evan Ayalin <siyeel04@gmail.com>
2025-06-16 16:10:36 +0800

    Created component - ButtonVideoGame

--- src/templates/auth/auth.js
+++ src/templates/auth/auth.js
@@ -1,2 +1,24 @@
 import '../../styles/main.css';
 import './auth.css'
+import '../../components/Buttons/ButtonStyle.css';
+
+import ButtonVideoGame from '../../components/Buttons/VideoGame/ButtonVideoGame.js';
+
+const Auth = function () {
+    const page = document.body.querySelector('#page__auth');
+    
+    // BUTTONS INITIALIZATIONS //
+    
+    const bottom_gate = page.querySelector('#bottom-gate');
+    const actions = bottom_gate.querySelector('.actions');
+
+    const createBtn = ButtonVideoGame('new account');
+    const loadBtn = ButtonVideoGame('load account');
+    const settingsBtn = ButtonVideoGame('settings');
+    
+    createBtn.render(actions);
+    loadBtn.render(actions);
+    settingsBtn.render(actions);
+    
+    // BUTTONS INITIALIZATIONS //
+}();
